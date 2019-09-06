aa7bb4e12abb2763c072aca67f144d8adbf713ee
oversj96 <45669103+oversj96@users.noreply.github.com>
2019-09-05 19:56:27 -0500

    refactored summation

--- Python/tribonacci_sequence.py
+++ Python/tribonacci_sequence.py
@@ -1,6 +1,6 @@
 def tribonacci(signature, n):
     for i in range(0, n-len(signature)):
-        signature.append(signature[i] + signature[i+1] + signature[i+2])
+        signature.append(sum(signature[-3:]))
     return signature[:n]
 
 print(tribonacci([1, 1, 1], 10))
