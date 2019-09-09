e95104bbf6fbc023b3538f11c3848b5c1e7f38f6
oversj96 <45669103+oversj96@users.noreply.github.com>
2019-09-09 17:24:09 -0500

    completed Twice Linear kata

--- Python/dbl_linear.py
+++ Python/dbl_linear.py
@@ -1,16 +1,29 @@
+import timeit
+
 def dbl_linear(n):
     list = [1]
+    i = 0
+    key = None
+    checkpoint = 0
+    checkpoint_val = n
     for x in list:
-        n1 = 2*x+1
-        n2 = 3*x+1
-        if(n1 not in list):
-            list.append(n1)
-        if(n2 not in list):
-            list.append(n2)
-        list.sort()
-        if(len(list) > n and x == list[n]):
-            break
-    return list[n]
+        list.append(2*x+1)
+        list.append(3*x+1)
+        if(i == checkpoint_val):
+            if(checkpoint == 3):
+                break
+            else:
+                checkpoint += 1
+                checkpoint_val = len(list)
+        i += 1
+    list.sort()
+    sorted_list = []
+    for y in list:
+        if(y != key):
+            sorted_list.append(y)
+        key = y
+    return sorted_list[n]
 
-print(dbl_linear(10000000))
-print('')
+print(timeit.timeit('dbl_linear(60000)', globals=globals(), number=1))
+
+# 694 Milliseconds to complete dbl_linear(60000)
