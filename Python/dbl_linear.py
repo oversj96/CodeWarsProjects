c77a15c361c15f351992a3d8c98e8bdf4a08bad6
oversj96 <45669103+oversj96@users.noreply.github.com>
2019-09-10 17:16:45 -0500

    old version

--- Python/dbl_linear.py
+++ Python/dbl_linear.py
@@ -1,20 +1,12 @@
 def dbl_linear(n):
     list = [1]
-    list2 = []
-    key = None
-    start = 0
-    for x in list:
-        n1 = 2*x+1
-        n2 = 3*x+1
-        list.append(n1)
-        list.append(n2)
-        if(len(list) > n+1 == 0):
-            list.sort()
-            for x in range(start, len(list)):
-                if(list[x] != key):
-                    list2.append(x)
-                key = list[x]
-            start = x
-    return list2[n-1]
+    result = []
+    i = 0
+    num = list[i]
+    n1 = 2*num+1
+    n2 = 3*num+1
+    list.append(n1)
+    list.append(n2)
+    return result[n-1]
         
-print(dbl_linear(10))
+print(dbl_linear(50))
